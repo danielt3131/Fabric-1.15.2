3c9428c6ae403e4fddac390d04fdc0d5f0ca2635
Daniel Thompson <danielt3131@gmail.com>
2020-02-21 13:04:02 -0500

    Added Corn

--- src/main/java/com/lsutigers3131/morefood/Main.java
+++ src/main/java/com/lsutigers3131/morefood/Main.java
@@ -15,7 +15,7 @@ public class Main implements ModInitializer
     public static final Item item_thicksteak = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(12).saturationModifier(16).meat().statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH,200), 0.5F).build()));
     public static final Item item_lightsaber = new item_lightsaber(new Item.Settings().group(ItemGroup.COMBAT).maxDamage(99999));
     public static final Item item_corn = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(ItemReference.item_corn_hunger).saturationModifier(ItemReference.item_corn_saturation).build()));
-
+// Registers items
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("morefood", "item_thicksteak"), item_thicksteak);
